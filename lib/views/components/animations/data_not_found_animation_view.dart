d7d75f33a88df9f0d686d87458678e5977a9e3a8
Peter Osas <osaspeter6@gmail.com>
2024-08-19 21:23:03 +0100

    added assets folder

---
+++ lib/views/components/animations/data_not_found_animation_view.dart
@@ -0,0 +1,6 @@
+import 'package:instantgram_clone/views/components/animations/lottie_animation_view.dart';
+
+class DataNotFoundAnimationView extends LottieAnimationView {
+  const DataNotFoundAnimationView({super.key})
+      : super(animation: 'assets/animations/data_not_found.json');
+}
---
+++ lib/views/components/animations/empty_content_animation_view.dart
@@ -0,0 +1,6 @@
+import 'package:instantgram_clone/views/components/animations/lottie_animation_view.dart';
+
+class EmptyContentAnimationView extends LottieAnimationView {
+  const EmptyContentAnimationView({super.key})
+      : super(animation: 'assets/animations/empty.json');
+}
