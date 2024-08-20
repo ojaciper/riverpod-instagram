31a5652de2ccd53741e1d6ea0609a390fc929bc8
Peter Osas <osaspeter6@gmail.com>
2024-08-20 04:54:13 +0100

    feat: added lottie animation component

---
+++ lib/views/components/animations/models/error_animation.dart
@@ -0,0 +1,6 @@
+import 'package:instantgram_clone/views/components/animations/lottie_animation_view.dart';
+
+class ErrorAnimation extends LottieAnimationView {
+  const ErrorAnimation({super.key})
+      : super(animation: 'assets/animations/error.json');
+}
---
+++ lib/views/components/animations/models/small_erorr_animation.dart
@@ -0,0 +1,6 @@
+import 'package:instantgram_clone/views/components/animations/lottie_animation_view.dart';
+
+class SmallErorrAnimation extends LottieAnimationView {
+  const SmallErorrAnimation({super.key})
+      : super(animation: 'assets/animations/small_error.json');
+}
